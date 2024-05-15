bf3bbd4caf34af51519eaea7af86a9255f2f2bf3
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-05-15 18:01:12 -0400

    imagentrabajando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="absolute top-0 left-0 w-full h-screen z-[-1]">
         <Image
-          src="/prueba/estamos-trabajando-impulsopixel.jpeg"
+          src="/pruebas/estamos-trabajando-impulsopixel.jpeg"
           alt="Estamos trabajando"
           fill
           quality={100}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority={true}
         />
       </div>
 

18975b22deb23ba141d3063e0b7fe2ef8d487337
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-05-15 21:11:02 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from 'next/head';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );

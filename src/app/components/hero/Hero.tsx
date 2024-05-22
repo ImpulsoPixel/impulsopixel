e1fbf70cbdfef8c33f7285526462976c31158dc6
Daniel Garcia Rojas <d.g.r.developer@gmail.com>
2024-05-21 22:01:19 -0400

    se agrega fuente para titulo

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -6,7 +6,8 @@ const Hero = () => {
         <div className={`${styles.hero} flex flex-col items-center justify-center text-center h-screen`}>
             <div className="flex flex-col items-center">
                 <Image src="/images/ImpulsoPixelLogo.jpg" alt="Impulso Pixel Logo" width={100} height={100} className="mb-4" />
-                <h1 className="text-4xl font-bold text-black mb-2">Impulso Pixel</h1>
+                <h1 className={`${styles.title} text-4xl font-bold text-black mb-2`}>Impulso Pixel</h1>
+
                 <p className="text-xl text-gray-600 mb-6">Diseñamos tu futuro digital, pixel a pixel</p>
             </div>
             <div className="flex space-x-4">

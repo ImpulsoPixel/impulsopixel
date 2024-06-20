5c2715ad16d019898817d2ca61c4ea8ef7080796
MxzGnTh <mixzio.gonzalez@gmail.com>
2024-06-20 17:58:45 -0400

    nav, footer , Seccion Servicios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import Image from 'next/image';
+
 import Hero from './components/hero/Hero';
+import Navbar  from './components/navbar/Navbar';
+import Servicios from './components/servicios/Servicios';
+import Footer from './components/footer/Footer';
 import './globals.css';
 
 
@@ -8,9 +11,11 @@ import './globals.css';
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero />
-     
-
+      <Servicios />
+      <Footer />
+      
       </div>
   
   );
